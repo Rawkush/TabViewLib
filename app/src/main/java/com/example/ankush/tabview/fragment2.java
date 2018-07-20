e29c634dcceb13716450a5616e044ea416d831e8
rawkush <ankushrawat950@gmail.com>
2018-07-20 15:52:37 +0530

    created fragment2, renamed one xml file

--- app/src/main/java/com/example/ankush/tabview/fragment2.java
+++ app/src/main/java/com/example/ankush/tabview/fragment2.java
@@ -1,14 +1,12 @@
 package com.example.ankush.tabview;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class fragment1 extends Fragment {
-
-
+public class fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
