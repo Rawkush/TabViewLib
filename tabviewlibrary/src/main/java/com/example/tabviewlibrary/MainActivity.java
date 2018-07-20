2f69fc0464f8451cbb64c6801e7f380d16f3f226
rawkush <ankushrawat950@gmail.com>
2018-07-20 14:27:10 +0530

    setting up other methods of adapter

---
+++ tabviewlibrary/src/main/java/com/example/tabviewlibrary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tabviewlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
