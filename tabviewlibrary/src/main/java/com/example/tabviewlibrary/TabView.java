8327ebfdab40c80b383c7878f7604cad120ffd57
rawkush <ankushrawat950@gmail.com>
2018-07-20 16:00:26 +0530

    refactoriung the name of xml file present inlibrary

--- tabviewlibrary/src/main/java/com/example/tabviewlibrary/TabView.java
+++ tabviewlibrary/src/main/java/com/example/tabviewlibrary/TabView.java
@@ -21,7 +21,7 @@ private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tabView);
+        setContentView(R.layout.activity_tab_viewlib12);
         init();
     }
 
