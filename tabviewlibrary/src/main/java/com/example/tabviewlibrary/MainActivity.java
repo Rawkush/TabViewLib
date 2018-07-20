fca0e98cbfe84c82fa19fe9e69b0d91e414a169f
rawkush <ankushrawat950@gmail.com>
2018-07-20 15:20:48 +0530

    addFragment function added

--- tabviewlibrary/src/main/java/com/example/tabviewlibrary/MainActivity.java
+++ tabviewlibrary/src/main/java/com/example/tabviewlibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tabviewlibrary;
 
 import android.arch.lifecycle.ViewModelStoreOwner;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 private CustomFragmentAdapter adapter;
 private List<FragmentModel> fragments;
 private ViewPager viewPager;
+private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +28,29 @@ private ViewPager viewPager;
 
     private void init(){
         fragments=new ArrayList<>();
+        initViewPager();
         initAdapter();
     }
 
-private void initAdapter(){
-    adapter = new CustomFragmentAdapter(getSupportFragmentManager(), fragments);
+    public void initViewPager(){
+        viewPager=(ViewPager)findViewById(R.id.viewpager);
+    }
 
+    private void initAdapter(){
+        adapter = new CustomFragmentAdapter(getSupportFragmentManager(), fragments);
+        viewPager.setAdapter(adapter);
 
-}
+    }
+
+    public void initTabView(){
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void addFragment(FragmentModel fragmentModel){
+        fragments.add(fragmentModel);
+        adapter.notifyDataSetChanged();
+    }
+
 
 }
